b68ac42c194df2aeb6a6af4805c0e5c0d9149304
Florian Pagnoux <florian.pagnoux@gmail.com>
2021-06-02 22:14:33 -0400

    Improve home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,24 @@
 import Container from "@material-ui/core/Container";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faShieldVirus,
+  faHome,
+  faBriefcase,
+  faUtensils,
+} from "@fortawesome/free-solid-svg-icons";
 
 import Layout from "../components/layouts/BaseLayout";
 
 import styles from "./Home.module.scss";
+
+const heroItems = [
+  { title: "Contact", icon: faEnvelope },
+  { title: "Unemployment", icon: faBriefcase },
+  { title: "Housing", icon: faHome },
+  { title: "Food Access", icon: faUtensils },
+  { title: "Covid", icon: faShieldVirus },
+];
 
 export default function Home() {
   return (
@@ -10,8 +26,11 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.hero}>
           <Container maxWidth="md" style={{ height: "100%" }}>
-            <div className="title-container">
-              <h1>Proudly serving Somerville</h1>
+            <div className={styles.iconBar}>
+              {heroItems.map(({ title, icon }) => (
+                <FontAwesomeIcon icon={icon} size="3x" />
+              ))}
+              {/* <h1>Proudly serving Somerville</h1> */}
             </div>
           </Container>
         </div>
