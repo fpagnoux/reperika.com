84e9e0a647d3bda8b42775c0315f2b45a446fb3d
Florian Pagnoux <florian.pagnoux@gmail.com>
2021-06-02 23:42:01 -0400

    Use frontmatter in md

--- components/layouts/IssueLayout.tsx
+++ components/layouts/IssueLayout.tsx
@@ -3,10 +3,11 @@ import Container from "@material-ui/core/Container";
 import BaseLayout from "./BaseLayout";
 import styles from "./IssueLayout.module.scss";
 
-export default function IssueLayout({ children }) {
+export default function IssueLayout({ children, frontMatter }) {
   return (
     <BaseLayout>
       <Container maxWidth="md" className={styles.main}>
+        <h1>{frontMatter.title}</h1>
         {children}
       </Container>
     </BaseLayout>
