a2680410e9009789ccd54b2e5b4621f87adb02ac
Florian Pagnoux <florian.pagnoux@gmail.com>
2021-06-06 13:13:41 -0400

    Allow to close newsletter

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "@material-ui/core/Link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 import NewsletterModal from "./NewsletterModal";
 import styles from "./Footer.module.scss";
@@ -7,17 +9,39 @@ import styles from "./Footer.module.scss";
 export default function Footer({}) {
   const [isNewsletterFormOpen, setIsNewsletterFormOpen] =
     useState<boolean>(false);
+  const [showBanner, setShowBanner] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && window != null) {
+      if (sessionStorage.getItem("hideNewsletterSignup")) {
+        setShowBanner(false);
+      }
+    }
+  }, []);
+
+  const hideBanner = () => {
+    setShowBanner(false);
+    sessionStorage.setItem("hideNewsletterSignup", "true");
+  };
+
   return (
     <>
       <div className={styles.placeholder}></div>
-      {!isNewsletterFormOpen && (
+      {!isNewsletterFormOpen && showBanner && (
         <footer className={styles.footer}>
-          <Link
-            component="button"
-            onClick={() => setIsNewsletterFormOpen(true)}
-          >
-            Sign up for the newsletter
-          </Link>
+          <div className={styles.footerCloseButton}>
+            <Link component="button" onClick={hideBanner}>
+              <FontAwesomeIcon icon={faTimes} size="2x" />
+            </Link>
+          </div>
+          <div className={styles.footerContent}>
+            <Link
+              component="button"
+              onClick={() => setIsNewsletterFormOpen(true)}
+            >
+              Sign up for the newsletter
+            </Link>
+          </div>
         </footer>
       )}
       {isNewsletterFormOpen && (
